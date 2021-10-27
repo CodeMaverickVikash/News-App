9a3aa02706ff5ea1bf8e863b7bec6fb7485e31b0
vikash <purposetesting92@gmail.com>
2021-10-27 15:25:37 +0530

    Add date and images

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Initialize the news api parameters
 let country = 'in';
-let apiKey = 'f684d2873c9c4016bd0a221f4e0bddf1'
+let apiKey = config.API_KEY; // Add YOUR_API_KEY
 
 // Grab the news container
 let newsAccordion = document.getElementById('newsAccordion');
@@ -27,21 +27,22 @@ xhr.onload = function () {
             let news = `<div class="card">
                             <div class="card-header" id="heading${index}">
                                 <h2 class="mb-0">
-                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-                                    aria-expanded="false" aria-controls="collapse${index}" >
+                                <a class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
                                    <b>Breaking News ${index + 1}:</b> ${element["title"]} - Published At: ${dat} ${time}
-                                </button>
+                                </a>
                                 </h2>
                             </div>
 
                             <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-                                <h1 style="margin-left: 29px;"><b>${element["title"]}</b></h1>
-                                <p style="margin-left: 28px; margin-top: 17px;">${element["description"]}</p> 
+                                <h3><b>${element["title"]}</b></h3>
+                                <p>${element["description"]}</p> 
 
-                                <div class="card-body" style="display: inline-block; width: 500px;"> <p style="margin-bottom: 30px;">${element["content"]}</p>. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
-                                <img src="${element["urlToImage"]}" style="width: 500px; margin-top: 12px; margin-bottom: 17px;">
+                                <div class="card-body"> <p>${element["content"]}</p>. <a href="${element['url']}" target="_blank" >Read more here</a>
+                                <img src="${element["urlToImage"]}">
+                                </div>
                             </div>
-                        </div>`;
+                        </div><hr>`;
             newsHtml += news;
         });
         newsAccordion.innerHTML = newsHtml;
